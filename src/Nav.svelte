46e6fb27499c95a6098dbfbbe868b3e45f0a2f85
Joris Van herrewegen <joris.vanherrewegen@gmail.com>
2021-08-31 21:43:45 +0200

    pagina's vragen, netwerk

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -2,13 +2,17 @@
     // main navigation for the application
     import Home from './pages/index.svelte';
     import Kaart from './pages/kaart.svelte';
-    //import Vragen from './pages/vragen.svelte';
+    import Vragen from './pages/vragen.svelte';
+    import Netwerk from './pages/netwerk.svelte';
     
     // these are our 'pages' to navigate through (in order for this export to work the script tag MUST include context="module")
     export const navOptions = [
           //  { page: 'About this app',   component: About },
         { page: 'Home',   component: Home },
        { page: 'Kaart',   component: Kaart },
+       { page: 'Vragen',   component: Vragen },
+       { page: 'Netwerk',   component: Netwerk },
+
             // other navigation pages can go here
     ];
     </script>
---
+++ src/pages/netwerk.svelte
@@ -0,0 +1,50 @@
+<h2>Netwerk van mensen</h2>
+
+<p>HOE ZIT EEN LETS-GROEP IN ELKAAR?</p>
+
+<div class="lets_cirkel">
+   <span class="prent"><img SRC="./images/lets_prent_affiche-2400.jpg" alt="Lets cirkel" >
+  </span>
+    <span class="tekst">
+        Miet is komen wonen in de streek. <br>
+        Omdat ze nieuwe mensen wil leren kennen, <br>
+        heeft ze zich aangesloten bij de LETS-groep van Regio Zottegem. <br>
+        Via deze groep kent ze nu Lucienne. <br>
+        <b>Lucienne</b> is al 1 jaar bij de groep aangesloten <br>
+        en helpt <b>Jesse</b> af en toe bij haar huiswerk. <br>
+         Ze wordt hiervoor gewaardeerd in ietsen. <br>
+        Omdat ze zelf moeilijk te been is, helpt <b>Miet</b><br>
+        &ndash; die niet zo ver van Lucienne woont&ndash; <br>
+        haar graag bij het boodschappen doen. <br>
+        Hiervoor wordt zij ook gewaardeerd in ietsen. <br>
+        Miet moet veel op de computer werken, <br>
+        maar aangezien zij daarmee soms wel problemen heeft, <br>
+        wil <b>Jan</b>, de computerbolleboos van de groep, <br>
+        haar graag helpen tegen ietsen, &hellip;<br>
+         Maar Jan is niet zo'n specialist in opruimen ;-) &hellip; <br>
+         daarvoor kan hij dus de hulp van <b>Frieda </b> goed gebruiken.<br>
+         Alles netjes maken is geen probleem voor Frieda , <br>
+        maar ze is heel blij dat ze voor het invullen van die <br>
+        moeilijke formulieren te rade kan gaan mij Mark.<br>
+        Papierenwerk is Mark zijn ding &hellip; schilderen niet echt &hellip; <br>
+        Maar gelukkig is Dede daar al om een handje te helpen.<br>
+         En &hellip; had je al door dat Jesse het dochtertje is van Dede?
+        <br> 
+        <br>Ziezo, de cirkel is rond !
+        <br>Bij het echte Letsen zal het natuurlijk geen mooie cirkel zijn,<br>
+        het zal eerder een web van transacties tussen de letsers zijn.
+    </span>
+</div>
+<style  scoped>
+    .lets_cirkel{
+        display: flex;
+        margin: 20px 0;
+    }
+
+   .prent {
+       width: 432px;
+   } 
+   .tekst {
+       width: 100%;
+   }
+</style>
